3fdc8d52b3567b9b063cce96716469a3ffcfcbce
adrianmehmeti <135590336+adrianmehmeti@users.noreply.github.com>
2024-01-15 09:33:46 +0100

    Add files via upload

---
+++ src/main/java/com/example/testinggithub/TestingGithubApplication.java
@@ -0,0 +1,13 @@
+package com.example.testinggithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestingGithubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestingGithubApplication.class, args);
+	}
+
+}
